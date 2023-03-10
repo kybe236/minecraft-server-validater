89d507a2f76f577fe2b23c12b49fbe8912a4afb2
kyrill belomenov <kyrill.belomenov@outlook.de>
2023-03-10 20:38:21 +0100

    added more description

--- main.py
+++ main.py
@@ -13,31 +13,34 @@ input_file = args.input_file
 # set list for ips
 masscan = []
 
-
+# open file and create a list of lines
 fileHandler = open(input_file, "r")
 listOfLines = fileHandler.readlines()
 fileHandler.close()
 
+# grep only the ip from the lines
 for line in listOfLines:
     if line.strip()[0] != "#":
         masscan.append(line.strip().split(' ', 4)[3])
 
 
+# split the array for avery thread
 def split_array(mas, n):
     return [mas[i::n] for i in range(n)]
 
 
+# wait for threat input
 threads = int(input('threads: '))
 
-
+# limit the threads to the length of the ips
 if len(masscan) < int(threads):
     threads = len(masscan)
 
+# use the split function
 split = list(split_array(masscan, threads))
 
-exitFlag = 0
 
-
+# class for threads
 class MyThread(threading.Thread):
     def __init__(self, threadid, name):
         threading.Thread.__init__(self)
@@ -46,22 +49,23 @@ class MyThread(threading.Thread):
 
     def run(self):
         print("Starting Thread " + self.name)
-        print_time(self.name)
+        check(self.name)
         print("Exiting Thread " + self.name)
 
 
-def print_time(threadname):
+# check the server via the mcstatus api
+def check(threadname):
     for z in split[int(threadname)]:
-        if exitFlag:
-            threadname.exit()
-            print("1")
         try:
+            # check if server is online
             ip = z
             server = JavaServer(ip, 25565)
             status = server.status()
         except Exception as ex:
+            # error behaviour
             print(f"er: {ex}")
         else:
+            # print and save found server
             print("Found server: " + ip + " " + status.version.name + " " + str(status.players.online) + " " + str(
                 status.players.max))
             text_file = open("out.txt", "a")
@@ -73,5 +77,6 @@ def print_time(threadname):
             text_file.close()
 
 
+# start the threads
 for x in range(threads):
     MyThread(x, str(x)).start()
